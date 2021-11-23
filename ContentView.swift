993105d452c284fe3b3ddbd98d9130e5f83080a9
Nerijus <surkeela@yahoo.com>
2021-11-23 14:54:09 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Nerijus Surkys on 2021-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Nerijus Surkys on 2021-11-23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

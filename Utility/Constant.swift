d92efcdf863b3bff016250663cebb0f6d4440297
Nerijus <surkeela@yahoo.com>
2021-12-15 12:58:24 +0200

    WIP:
    add footerView

---
+++ Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Nerijus Surkys on 2021-12-15.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorgray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC

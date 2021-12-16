cd4b8037a96949822c09247f9092d7dba22753c4
Nerijus <surkeela@yahoo.com>
2021-12-16 11:12:54 +0200

    WIP: vertical grid layout

---
+++ View/Home/TitleView.swift
@@ -0,0 +1,39 @@
+//
+//  TitleView.swift
+//  Touchdown
+//
+//  Created by Nerijus Surkys on 2021-12-16.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    // MARK: - PROPERTY
+    
+    var title: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.largeTitle)
+            .fontWeight(.heavy)
+            
+            Spacer()
+        } //: HSTACK
+        .padding(.horizontal)
+        .padding(.top, 15)
+        .padding(.bottom, 10)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView(title: "Helmet")
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}

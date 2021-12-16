bd2323732b7ec2e88495fbeffab49d659bd00aa5
Nerijus <surkeela@yahoo.com>
2021-12-16 11:41:58 +0200

    WIP:
    horizontal grid layout for the brands

---
+++ Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Nerijus Surkys on 2021-12-16.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
